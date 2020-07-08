4bc7f091c78e8a07d45536d1e4fb3e614ded947f
Timo Schwarzer <me@timoschwarzer.com>
2020-07-08 12:18:14 +0200

    Fix migration class name

--- database/migrations/2020_05_06_101506_create_transferred_files_table.php
+++ database/migrations/2020_05_06_101506_create_transferred_files_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTransferFieldsTable extends Migration
+class CreateTransferredFilesTable extends Migration
 {
     /**
      * Run the migrations.
